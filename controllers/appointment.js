15ba827bef21de085f177746285b85883b03a3a1
Massicilia <massicilia.nedir@outlook.com>
2020-07-13 02:31:31 +0200

    error appointment.js

--- controllers/appointment.js
+++ controllers/appointment.js
@@ -1,19 +1,5 @@
 var AppointmentService = require('../services/appointment')
 module.exports = {
-    /* getAppointments: async function (req, res, next) {
-        // Validate request parameters, queries using express-validator
-
-        try {
-            var appointments = await AppointmentService.getAppointments({})
-            return res.status(200).json({
-                status: 200,
-                data: appointments,
-                message: "Succesfully Appointments Retrieved"
-            });
-        } catch (e) {
-            return res.status(400).json({status: 400, message: e.message});
-        }
-    }, **/
 
     create: (req, res, next) => {
         AppointmentService.postAppointment(req, res);
--- services/appointment.js
+++ services/appointment.js
@@ -27,25 +27,6 @@ exports.postAppointment = async function (req, res, options) {
     }
 }*/
 module.exports = {
-   /* postAppointment: (req, res) =>
-    {
-        return new Promise((next) => {
-            return db.appointment.create(
-                {
-                    reason: req.body.reason,
-                    date: req.body.date,
-                    veterinary_nordinal: req.body.veterinary_nordinal,
-                    petowner_idpetownerappoint: req.body.petowner_idpetownerappoint,
-                    pet_idpetappoint: req.body.pet_idpetappoint
-                }
-            )
-        .then((result) => {
-            console.log(result)
-        })
-        .catch((err) => next(err))
-        })
-        //NOTIFICATIONS A ENVOYER
-    }, **/
 
     postAppointment: (req,res) =>
     {
@@ -80,54 +61,3 @@ module.exports = {
             //NOTIFICATIONS A ENVOYER
     }
 }
-
-/**
-var express = require('express');
-var router = express.Router();
-const db = require('../models');
-
-
-router.get('/', function(req, res, next) {
-    db.appointment.findAll().then(appointment => {
-        res.send(appointment);
-    })
-        .catch(error => res.json({
-            error: true,
-            data: [],
-            error: error
-        }));
-});
-*/
-/**
- * @POST /appointment/
- */
-/**
-router.post('/', function(req, res, next) {
-    const {
-        reason,
-        date,
-        veterinary_nordinal,
-        petowner_idpetownerappoint,
-        pet_idpetappoint
-    } = req.body;
-
-    db.appointment.create({
-        reason: reason,
-        date: date,
-        veterinary_nordinal: veterinary_nordinal,
-        petowner_idpetownerappoint: petowner_idpetownerappoint,
-        pet_idpetappoint: pet_idpetappoint
-    })
-        .then(veterinary => res.status(201).json({
-            error: false,
-            data: appointment,
-            message: 'New appointment created.'
-        }))
-        .catch(error => res.json({
-            error: true,
-            data: [],
-            error: error
-        }));
-});
-module.exports = router;
-*/
