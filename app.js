e27773ff6e15f9b4fe3962fca0a3f0e9ca66b0fe
Massicilia <massicilia.nedir@outlook.com>
2020-08-02 15:29:16 +0200

    cors access blocked

--- app.js
+++ app.js
@@ -26,12 +26,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // CORS ACCESS
-router.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+app.post('/api/status', function (req, res, next) {
+  // your code goes here
+});
+
+module.exports = app;
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -61,8 +66,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-
 
 module.exports = app;
 
