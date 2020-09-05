4652ac634db5c61d07f949a6ffbf9a072a27a172
Massicilia <massicilia.nedir@outlook.com>
2020-09-05 15:58:39 +0200

    consultation.js list

--- controllers/consultation.js
+++ controllers/consultation.js
@@ -1,15 +1,20 @@
 var handler = require('../handlers/crudHandlers');
 var model = require('../models')
+var consultationmodel = model.consultation;
 module.exports = {
 
-    /**
-     *
-     * @param req
-     * @param res
-     * @param next
-     */
-    getAll: (req, res) => {
-        var consultationmodel = model.consultation;
-        handler.getAll(req,res,consultationmodel,{});
-    }
+    getAll: (req,res) => {
+        handler.getAll(req, res, consultationmodel, {})
+            .then(function (consultationsFound) {
+                if (consultationsFound != null) {
+                    return res.status(200).json(consultationsFound);
+                }
+                else {
+                    return res.status(404).json({'error': 'consultations do not exist in DB'});
+                }
+            })
+            .catch(function (err) {
+                return res.status(500).json({'error': 'Unable to get the consultations'})
+            })
+    },
 }
