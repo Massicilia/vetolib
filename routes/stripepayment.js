6a75eccf6718bd1ef647c2b9bec6c41a50089086
Massicilia <massicilia.nedir@outlook.com>
2020-09-29 19:50:15 +0200

    stripe payment on setup intent

--- routes/stripepayment.js
+++ routes/stripepayment.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 var StripePaymentController = require('../controllers/example')
-router.route('public-key')
+router.route('/public-key')
     .get(StripePaymentController.getPublicKey)
-router.route('create-setup-intent')
+router.route('/create-setup-intent')
     .post(StripePaymentController.createSetupIntent)
 
 module.exports = router;
