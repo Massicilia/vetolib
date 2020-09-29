cf816f83843d2beb61abcc375fa07f38007d6f6d
Massicilia <massicilia.nedir@outlook.com>
2020-09-29 03:32:40 +0200

    correct registration credit card on payment module

--- routes/stripepayment.js
+++ routes/stripepayment.js
@@ -2,8 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 var StripePaymentController = require('../controllers/stripepayment')
-router.route('')
-    .post(StripePaymentController.create)
 router.route('/all')
     .get(StripePaymentController.getAllCreditCards)
 router.route('')
