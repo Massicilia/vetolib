20ce7f160eb9c03b342fbce04e8519e2d9cea57e
Massicilia <massicilia.nedir@outlook.com>
2020-09-29 19:59:00 +0200

    stripe payment on setup intent

--- controllers/example.js
+++ controllers/example.js
@@ -1,5 +1,5 @@
 const stripe = require('stripe')('sk_test_51HM2DTGVBJFFbfQTXQ1RJ3FA6Jn7e7wdjEVguo9HBVUvPX4mdmijMSmm51NxwsBU27VcJuMaWpiS6b1UcVTlNArY00I7TYtrWJ');
-
+const stripepk = Stripe('pk_test_51HM2DTGVBJFFbfQT6801jISmTHsxuGPrL9Icrrun7AgBKbobG0MaxcUPm9anfnA60U53L7gX3RTMN0hxdxnFq8tQ00VRODNzsZ'); // use your test publishable key
 module.exports = {
 //app.get("/", (req, res) => {
     get: (req, res) => {
@@ -9,7 +9,7 @@ module.exports = {
 
 //app.get("/public-key", (req, res) => {
     getPublicKey: (req, res) => {
-        res.send({publicKey: process.env.STRIPE_PUBLISHABLE_KEY});
+        res.send({publicKey: stripepk});
     },
 
 //app.post("/create-setup-intent", async (req, res) => {
