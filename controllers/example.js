fc4e69c836a3556bafdb9621ade23f3bcc955600
Massicilia <massicilia.nedir@outlook.com>
2020-09-30 00:04:05 +0200

    stripe payment on setup intent

--- controllers/example.js
+++ controllers/example.js
@@ -1,5 +1,7 @@
 const stripe = require('stripe')('sk_test_51HM2DTGVBJFFbfQTXQ1RJ3FA6Jn7e7wdjEVguo9HBVUvPX4mdmijMSmm51NxwsBU27VcJuMaWpiS6b1UcVTlNArY00I7TYtrWJ');
 const stripepk = Stripe('pk_test_51HM2DTGVBJFFbfQT6801jISmTHsxuGPrL9Icrrun7AgBKbobG0MaxcUPm9anfnA60U53L7gX3RTMN0hxdxnFq8tQ00VRODNzsZ'); // use your test publishable key
+const model = require('../models');
+const veterinarymodel = model.veterinary;
 module.exports = {
 //app.get("/", (req, res) => {
     get: (req, res) => {
@@ -14,12 +16,18 @@ module.exports = {
 
 //app.post("/create-setup-intent", async (req, res) => {
     createSetupIntent: async (req, res) => {
-        // Create or use an existing Customer to associate with the SetupIntent.
-        // The PaymentMethod will be stored to this Customer for later use.
-        const customer = await stripe.customers.create();
-
-        res.send(await stripe.setupIntents.create({
-            customer: customer.id
-        }));
+        handler.getOne({
+            where: {
+                nordinal: req.query.veterinary_nordinal
+            }
+        }, veterinarymodel)
+            .then(async function (veterinary) {
+                res.send(await stripe.setupIntents.create({
+                    customer: veterinary.customerID
+                }));
+            })
+            .catch(function (err) {
+                console.log('error : '+ err);
+            })
     }
 }
