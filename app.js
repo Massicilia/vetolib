52b4bf052694d6087aa9e1f72d0a8f7ba63628c8
Massicilia <massicilia.nedir@outlook.com>
2020-09-30 07:02:47 +0200

    CORS ERROR

--- app.js
+++ app.js
@@ -87,11 +87,16 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-// CORS ACCESS
+/* CORS ACCESS
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,DELETE,PATCH,HEAD,OPTIONS,POST,PUT");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+  next();
+});*/
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
