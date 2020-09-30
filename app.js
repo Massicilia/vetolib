f5f350e69cf76ff9b0545b19b3b43f0a832791fd
Massicilia <massicilia.nedir@outlook.com>
2020-09-30 06:23:16 +0200

    stripe payment on setup intent

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.set('view engine', 'pug');
 // CORS ACCESS
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,FETCH,PATCH,OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
