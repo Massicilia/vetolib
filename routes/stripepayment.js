4fba3ce6e326d37b36bf01b6749a452268bf6a4f
Massicilia <massicilia.nedir@outlook.com>
2020-09-30 08:25:45 +0200

    webhook stripe post request

--- routes/stripepayment.js
+++ routes/stripepayment.js
@@ -4,5 +4,7 @@ var router = express.Router();
 var StripePaymentController = require('../controllers/example2')
 router.route('/createsetupintent')
     .post(StripePaymentController.createSetupIntent2)
+router.route('/webhook')
+    .post(StripePaymentController.webhookHandler)
 
 module.exports = router;
