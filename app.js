421b85607cd85c794ea26eb1620e8a4dabedba69
Massicilia <massicilia.nedir@outlook.com>
2020-10-21 20:04:07 +0200

    CORS ERROR

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.use(function(err, req, res) {
 });
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*, http://localhost:4200");
   res.header('Access-Control-Allow-Credentials', true);
   res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH, HEAD, OPTIONS, PUT");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
