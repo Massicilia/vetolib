b4befc990c05d1fa0703c89e0230a22299ea4b4f
Massicilia <massicilia.nedir@outlook.com>
2020-10-21 21:49:35 +0200

    CORS ERROR

--- app.js
+++ app.js
@@ -26,17 +26,12 @@ const app = express();
 
 if (process.env.NODE_ENV === 'development') {
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:4200/login');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // headers authorization for preflight requests
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
-
-    app.options('*', (req, res) => {
-      // allowed XHR methods
-      res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
-      res.send();
-    });
   });
 }
 
